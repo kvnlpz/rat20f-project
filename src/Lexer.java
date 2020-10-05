55dacf5fd7cdb01b788e96062e762d61b75d73a2
kevinlopez8554 <kevinlopez8554@csu.fullerton.edu>
2020-10-04 23:05:48 -0700

    temporary update for downling code to another machine

--- src/Lexer.java
+++ src/Lexer.java
@@ -11,6 +11,11 @@ public class Lexer {
         this.input = input;
     }
 
+
+    private void matchFunction() {
+        //NUMBER OPERATOR WHITESPACE KEYWORD IDENTIFIER SEPARATOR
+    }
+
     public static ArrayList<Token> lexFunc(String input) {
         // The tokens
         ArrayList<Token> tokens = new ArrayList<Token>();
@@ -20,67 +25,59 @@ public class Lexer {
         //StringBuffer is mutable
         StringBuffer tokenBuffer = new StringBuffer();
         //iterate through all the tokens we have
-        for (TokenType tokenType : TokenType.values()){
+        for (TokenType tokenType : TokenType.values()) {
             tokenBuffer.append(String.format("|(?<%s>%s)", tokenType.name(), tokenType.pattern));
         }
         //regex patterns
+        matcher(input, tokens, tokenBuffer);
+        return tokens;
+    }
+
+    private static void matcher(String input, ArrayList<Token> tokens, StringBuffer tokenBuffer) {
         Pattern tokenPatterns = Pattern.compile(tokenBuffer.substring(1));
         // Now we're going to match the tokens with our enums
         Matcher matcher = tokenPatterns.matcher(input);
+        TokenType[] tokenTypeArrayList = new TokenType[]{TokenType.KEYWORD, TokenType.NUMBER, TokenType.IDENTIFIER, TokenType.SEPARATOR, TokenType.OPERATOR};
+        while (matcher.find()){
+            for(TokenType tokenType : tokenTypeArrayList){
+                if(matcher.group(tokenType.getType() == tok)){
+
+                }
+            }
+        }
+
         while (matcher.find()) {
             if (matcher.group(TokenType.KEYWORD.name()) != null) {
                 tokens.add(new Token(TokenType.KEYWORD, matcher.group(TokenType.KEYWORD.name())));
-                continue;
-            }
-            else if ((matcher.group(TokenType.IDENTIFIER.name()) != null) ) {
+            } else if ((matcher.group(TokenType.IDENTIFIER.name()) != null)) {
                 tokens.add(new Token(TokenType.IDENTIFIER, matcher.group(TokenType.IDENTIFIER.name())));
-                continue;
-            }
-
-
-            else if (matcher.group(TokenType.WHILESTATEMENT.name()) != null) {
-                tokens.add(new Token(TokenType.WHILESTATEMENT, matcher.group(TokenType.WHILESTATEMENT.name())));
-                continue;
-            }
-
-            else if (matcher.group(TokenType.IFSTATEMENT.name()) != null) {
-                tokens.add(new Token(TokenType.IFSTATEMENT, matcher.group(TokenType.IFSTATEMENT.name())));
-                continue;
-            }
-            else if (matcher.group(TokenType.SEPARATOR.name()) != null) {
+            }else if (matcher.group(TokenType.SEPARATOR.name()) != null) {
                 tokens.add(new Token(TokenType.SEPARATOR, matcher.group(TokenType.SEPARATOR.name())));
-                continue;
-            }
-
-
-            else if (matcher.group(TokenType.NUMBER.name()) != null) {
+            } else if (matcher.group(TokenType.NUMBER.name()) != null) {
                 tokens.add(new Token(TokenType.NUMBER, matcher.group(TokenType.NUMBER.name())));
-                continue;
-            }
-            else if (matcher.group(TokenType.OPERATOR.name()) != null) {
+            } else if (matcher.group(TokenType.OPERATOR.name()) != null) {
                 tokens.add(new Token(TokenType.OPERATOR, matcher.group(TokenType.OPERATOR.name())));
-                continue;
-            } else if (matcher.group(TokenType.WHITESPACE.name()) != null)
-                continue;
-
+            }
         }
-        return tokens;
     }
 
 
     public enum TokenType {
         // The types of tokens we can have
-        NUMBER("[0-9]*\\.?[0-9]"), OPERATOR("[*|/|+|-]"), WHITESPACE("[ \t\f\r\n]+"),
-        IFSTATEMENT("if\\([^|&\\n]*?\\)"),
+        NUMBER("[0-9]*\\.?[0-9]"),
+        OPERATOR("[*|/|+|-]"),
+        WHITESPACE("[ \t\f\r\n]+"),
         KEYWORD("(?<![a-zA-Z0-9])(if|while|int|get|for)(?![a-zA-Z0-9])"),
-        WHILESTATEMENT("while\\([^|&\\n]*?\\)"),
-//        IDENTIFIER("[_a-zA-Z][_a-zA-Z0-9]{0,30}")
-        IDENTIFIER("\\b(?!(one|if|while|int|get|for)\\b)\\w+"),
+        IDENTIFIER("\\b(?!(if|while|int|get|for)\\b)\\w+"),
         SEPARATOR("[^a-zA-Z\\d\\s:]");
         public final String pattern;
 
         TokenType(String pattern) {
             this.pattern = pattern;
+        }
+
+        public String getType() {
+            return pattern;
         }
     }
 
@@ -97,7 +94,7 @@ public class Lexer {
 
         @Override
         public String toString() {
-            return  type.name() + " " + data;
+            return type.name() + " " + data;
         }
     }
 
